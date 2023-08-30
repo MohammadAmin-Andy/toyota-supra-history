46dbef51b337acf997608be174787e977d0befcc
Mamin <pg.mohammadamin@gmail.com>
2023-08-30 04:10:02 +0330

    start page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:toyota_supra_info/start_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Application());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Avenir'),
+      home: StartPage(),
+    );
   }
 }
---
+++ lib/start_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 164, 69, 13),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'TOYOTA Supra History',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 65, 7, 3),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0),
+                  ),
+                  SizedBox(height: 20.0),
+                  ClipRRect(
+                    child: Image.asset('images/tuning.jpg'),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  SizedBox(height: 10.0),
+                  Center(
+                    child: SvgPicture.asset('images/Toyota_Supra.svg',
+                        height: 80.0),
+                  ),
+                  SizedBox(height: 30.0),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'About TOYOTA company',
+                      textDirection: TextDirection.ltr,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Color.fromARGB(255, 65, 7, 3),
+                        minimumSize: Size(120, 45),
+                        elevation: 5),
+                  ),
+                  SizedBox(height: 4),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.black,
+                      side: BorderSide(color: Colors.black),
+                      shadowColor: Colors.black,
+                      minimumSize: Size(225, 45),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'View Generations',
+                      textDirection: TextDirection.ltr,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      maximumSize: Size(150, 40),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Star to this project',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
